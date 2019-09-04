6bdf06d0169e56a5990db962711a23dab71a127e
ymorales-galvan <ymorales-galvan@mail.csuchico.edu>
2019-09-04 16:32:58 -0700

    Changes the greetings

--- hello.cpp
+++ hello.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 #include <string>
 
+//This is a single-line comment because it follows the two forward-slashes.
+//Yuliana Morales
+
 int main() {
     std::string input;
-    std::cout << "What's your first name?" << std::endl;
+    std::cout << "Hi, I'm Yuliana.What's your first name?" << std::endl;
     std::cin >> input;
     std::cout << "Hello, " << input << std::endl;
     return 0;
